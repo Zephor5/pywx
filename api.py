705525205b26a9f578100f22e8071de8a7f41468
Zephor5 <zephor@qq.com>
2016-08-31 11:05:04 +0800

    remove old blog

--- api.py
+++ api.py
@@ -12,7 +12,11 @@ app = flask.Flask('pywx')
 def blogs(wx_id=None):
     res = {}
     if wx_id:
-        cur = CONN.execute('select * from blog where name=?', (wx_id,))
+        CONN.execute("delete from blog where (select count(id) from blog where name=?)> 100 and id in "
+                     "(select id from blog where name=? order by save_time desc limit "
+                     "(select count(id) from blog where name=?) offset 100)", (wx_id, wx_id, wx_id))
+        CONN.commit()
+        cur = CONN.execute('select * from blog where name=? order by save_time desc limit 20', (wx_id,))
     else:
         cur = CONN.execute('select * from blog limit 100')
     for _, name, title, url, t in cur:
