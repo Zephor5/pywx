918a1aa5fc3626124bb4c50506b7409714d5ba36
Zephor5 <zephor@qq.com>
2016-09-01 10:20:21 +0800

    bugfix

--- module/blog.py
+++ module/blog.py
@@ -24,16 +24,14 @@ class Blog(object):
         for item in dom.iter('item'):
             title = item.findtext('title')
             url = item.findtext('url')
-            _try = True
-            while _try:
-                try:
-                    DB.execute("INSERT INTO blog (NAME,TITLE,URL) VALUES (?, ?, ?)", (alias, title, url))
-                except sqlite3.IntegrityError:
-                    logger.warn(u'%s 的 %s: %s 重复记录' % (alias, title, url))
-                except sqlite3.OperationalError:
-                    logger.error(u'%s 的 %s: %s 记录失败' % (alias, title, url))
-                else:
-                    added = True
+            try:
+                DB.execute("INSERT INTO blog (NAME,TITLE,URL) VALUES (?, ?, ?)", (alias, title, url))
+            except sqlite3.IntegrityError:
+                logger.warn(u'%s 的 %s: %s 重复记录' % (alias, title, url))
+            except sqlite3.OperationalError:
+                logger.error(u'%s 的 %s: %s 记录失败' % (alias, title, url))
+            else:
+                added = True
         if added:
             DB.commit()
         return added
