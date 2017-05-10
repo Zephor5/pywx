eb5d03cc39d882ee57bc4ac191d777c0800f4d3b
Zephor5 <zephor@qq.com>
2017-05-10 11:56:50 +0800

    fix encoding

--- module/client.py
+++ module/client.py
@@ -522,7 +522,8 @@ class WxClient(object):
         # purge cache for wxdata
         # noinspection PyBroadException
         try:
-            res = yield treq.get(u'http://wxspider.pub.sina.com.cn:8086/_purge/v1/wxspider/blogs/%s' % alias, timeout=3)
+            res = yield treq.get('http://wxspider.pub.sina.com.cn:8086/_purge/v1/wxspider/blogs/%s' %
+                                 alias.encode('utf-8'), timeout=3)
             content = yield res.content()
             if content.find('Successful purge') > 0:
                 self._notice_log('purge cache ok')
